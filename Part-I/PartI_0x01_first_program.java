2c2ed447e019ac1e663a194387a56939d90296a0
Nimish Dudhe <20cs3043@rgipt.ac.in>
2023-04-12 23:19:45 +0530

    First Program Added

---
+++ Part-I/PartI_0x01_first_program.java
@@ -0,0 +1,13 @@
+/*
+	This is a simple Java program.
+	This prints "Hello World!"
+
+*/
+
+class PartI_0x01_first_program{
+
+	public static void main(String args[]){
+		System.out.println("Hello World!");
+	}
+
+}
