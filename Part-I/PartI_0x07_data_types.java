9142e4fd7dc3e5f34ef936b5c4bc5a2d511ad15c
Nimish Dudhe <20cs3043@rgipt.ac.in>
2023-04-15 21:06:09 +0530

    Primitive Data Types

---
+++ Part-I/PartI_0x07_data_types.java
@@ -0,0 +1,43 @@
+class PartI_0x07_data_types{
+	public static void main(String args[]){
+
+	// Integer Data Types(long - 64 bit, int - 32 bit, short - 16 bit, byte - 8 bit)
+	byte a = 127;
+	System.out.println(a);
+
+	short b = 32767;
+	System.out.println(b);
+
+	int c = 2147483647;
+	System.out.println(c);
+
+	long d = 2147483647;
+	System.out.println(d);
+
+	// Floating-point Numbers(float, double)
+	float pi = (float)3.1416;
+	double r = 15.42375;
+	System.out.println(pi);
+
+	double area = pi*r*r;
+	System.out.println(area);
+
+	// Characters(char)
+	char c1 = 'n';
+	char c2 = 'v';
+	char c3 = 65;
+
+	System.out.println(c1);
+	System.out.println(c2);
+	System.out.println(c3);
+
+
+	// Boolean(boolean)
+	boolean bool1 = true;
+	boolean bool2 = false;
+
+	System.out.println(bool1 + " " + bool2);
+
+	}
+
+}
